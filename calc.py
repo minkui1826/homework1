8288a01b0b36dfa8aa8c3af74fc9cab9524221b2
minkui1826 <minkui1826@kyonggi.ac.kr>
2024-09-26 17:59:40 +0900

    feature added

--- calc.py
+++ calc.py
@@ -4,7 +4,15 @@ def add(x, y):
 def sub(x,y):
     return x-y
 
+def mul(x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
 a,b = map(float, input().split())
 
 print(f"Add: {add(a,b)}")
 print(f"Sub: {sub(a,b)}")
+print(f"Mul: {mul(a,b)}")
+print(f"Div: {div(a,b)}")
